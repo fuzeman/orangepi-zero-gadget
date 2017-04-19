d64cf89036cbc44bba118be40fb2e1f9ef86c55a
Dean Gardiner <me@dgardiner.net>
2017-04-20 10:55:32 +1200

    Updated snapcraft credential decryption parameters

--- .travis/decrypt.sh
+++ .travis/decrypt.sh
@@ -5,7 +5,7 @@ mkdir .snapcraft/
 
 # Decrypt snapcraft credentials
 openssl aes-256-cbc \
-  -K $encrypted_f4fc35a3a47c_key \
-  -iv $encrypted_f4fc35a3a47c_iv \
+  -K $encrypted_11c168ff89cc_key \
+  -iv $encrypted_11c168ff89cc_iv \
   -in .travis/snapcraft.cfg \
   -out .snapcraft/snapcraft.cfg -d
